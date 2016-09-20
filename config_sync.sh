98348abc399e415d8766d651bb19bde868170fcc
dhf <a@dhf.im>
2016-09-20 14:34:47 +0800

    Update config_sync.sh

--- config_sync.sh
+++ config_sync.sh
@@ -1,20 +1,37 @@
 #! /bin/bash
 
-if [ -f "./config_sync_var.sh" ]; then
-    source "./config_sync_var.sh"
+set -e
+
+usage() {
+    echo "Usage: sh $0 <configFile>"
+    exit 1
+}
+
+# ----------------------------------------
+
+if [ $# -lt 1 ]; then
+    usage
 fi
 
-CONFIG_FILE_PATH=${CONFIG_FILE_PATH:-"./server.properties"}
-FINAL_CONFIG_FILE=${1:-"${CONFIG_FILE_PATH}"}
+CONFIG_FILE=$1
+
+if [ -f "$CONFIG_FILE" ]; then
+    source "$CONFIG_FILE"
+else
+    echo "config file not found: $CONFIG_FILE"
+    usage
+fi
+
+PROPERTIES_FILE_PATH=${CONFIG_FILE_PATH:-"./server.properties"}
 
 # sync server configurations to consul kv
-if [ -f "$FINAL_CONFIG_FILE" ]; then
+if [ -f "$PROPERTIES_FILE_PATH" ]; then
     CONSUL_HOST=${CONSUL_HOST:-"127.0.0.1"}
     CONSUL_PORT=${CONSUL_PORT:-8500}
     CONSUL_KV_URL=http://"$CONSUL_HOST":"$CONSUL_PORT"/v1/kv
     CONFIG_KEY_PREFIX=${CONFIG_KEY_PREFIX:-"${RPC_SERVER_NAME}."}
 
-    awk -F '=' '{print $1 " " $2}' "$FINAL_CONFIG_FILE" | while read conf_key conf_value ; do
+    awk -F '=' '{print $1 " " $2}' "$PROPERTIES_FILE_PATH" | while read conf_key conf_value ; do
     syncSucc=$(curl --silent -X PUT "${CONSUL_KV_URL}/${CONFIG_KEY_PREFIX}${conf_key}" -d "${conf_value}")
     if [ "$syncSucc" != "true" ]; then
         echo "sync conf fail: {$conf_key : $conf_value }"
@@ -22,6 +39,6 @@ if [ -f "$FINAL_CONFIG_FILE" ]; then
     fi
     done
 else
-    echo "config file not found: $FINAL_CONFIG_FILE"
+    echo "properties file not found: $PROPERTIES_FILE_PATH"
     exit 127
 fi
